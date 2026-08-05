fdc1dc61083d5591f50fab76ed3ef6a3b3e2d58d
luismanfredi <luismanfredi920@gmail.com>
2026-08-05 13:38:52 -0300

    feat: add Matrix dimension getter

--- include/orion/matrix.hpp
+++ include/orion/matrix.hpp
@@ -33,4 +33,24 @@ class Matrix {
             }
             return os;
         }
+
+        friend std::ostream& operator<<(std::ostream& os, const Matrix& mat) { 
+            for(std::size_t i = 0; i < mat.rows_; i++){
+                os << "|";
+
+                for(std::size_t j = 0; j < mat.cols_; j++){
+                    os << std::setw(4)<< mat.data_[i][j];
+                }
+            os << " |\n";
+            }
+            return os;
+        }
+
+        std::size_t rows() const{
+            return rows_;
+        }
+
+        std::size_t cols() const{
+            return cols_;
+        }
 };

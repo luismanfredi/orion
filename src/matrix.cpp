d4946d4ad439ef675a1a82c98dc7323001fb688d
luismanfredi <luismanfredi920@gmail.com>
2026-08-06 09:03:23 -0300

    feat: add sum operator

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -58,6 +58,21 @@ std::size_t Matrix::cols() const {
     return cols_;
 }
 
+Matrix Matrix::operator+(const Matrix& other) const {
+    if (rows_ != other.rows_ || cols_ != other.cols_) {
+        throw InvalidMatrixDimensions("Matrix must be same dimensions!");
+    }
+
+    Matrix result(rows_, cols_);
+
+    for (std::size_t i = 0; i < rows_; i++) {
+        for (std::size_t j = 0; j < cols_; j++) {
+            result.set(i, j, get(i, j) + other.get(i, j));
+        }
+    }
+    return result;
+}
+
 Matrix Matrix::transpose() const{
     Matrix result(cols_, rows_);
 

5c9e476437c1178f53c9abc130d90c3b216923f0
luismanfredi <luismanfredi920@gmail.com>
2026-08-06 21:14:54 -0300

    feat: update basic examples

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -4,22 +4,29 @@
 int main() {
     Matrix A{
         {1, 2, 3},
-        {4, 5, 6}
+        {4, 5, 6},
+        {7, 8, 9}
     };
 
-    Matrix B{
-        {7, 8, 9},
-        {10, 11, 12}
-    };
+    Matrix B(3, 3, 1.5);
 
     std::cout << "Matrix A:\n";
     std::cout << A << "\n\n";
+
+    std::cout << "Rows A: " << A.rows() << "\n\n";
+    std::cout << "Columns A: " << A.cols() << "\n\n";
 
     std::cout << "Matrix B:\n";
     std::cout << B << "\n\n";
 
     std::cout << "A + B:\n";
     std::cout << A + B << "\n\n";
+
+    std::cout << "A * B:\n";
+    std::cout << A * B << "\n\n";
+
+    std::cout << "A * 2:\n";
+    std::cout << A * 2 << "\n\n";
 
     std::cout << "Transpose A:\n";
     std::cout << A.transpose() << "\n";

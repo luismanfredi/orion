d80fb67d3ab360a49311657b4022f01cadeb293d
luismanfredi <luismanfredi920@gmail.com>
2026-08-08 19:56:32 -0300

    refactor(matrix): remove set() an get() methods and refactor to use the call operator

--- include/orion/matrix.hpp
+++ include/orion/matrix.hpp
@@ -20,9 +20,6 @@ class Matrix {
   Matrix(std::size_t r, std::size_t c, double initial_value = 0.0);
   Matrix(std::initializer_list<std::initializer_list<double>> values);
 
-  void set(std::size_t r, std::size_t c, double val);
-  double get(std::size_t r, std::size_t c) const;
-
   void fill(double value = 0.0);
   void fillRange(double start, double step = 1.0);
 

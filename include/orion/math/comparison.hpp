74ecb62386c8af29a3e872707b7c1b490d290ced
luismanfredi <luismanfredi920@gmail.com>
2026-08-19 14:40:52 -0300

    refactor: change constants (abs_tol and rel_tol) to use the contributing standard

--- include/orion/math/comparison.hpp
+++ include/orion/math/comparison.hpp
@@ -7,7 +7,7 @@
 
 namespace orion {
 
-inline bool nearlyEqual(double a, double b, double absTol = ABS_TOL, double relTol = REL_TOL) {
+inline bool nearlyEqual(double a, double b, double absTol = kAbsTol, double relTol = kRelTol) {
   const double diff = std::abs(a - b);
 
   return diff <= std::max(absTol, relTol * std::max(std::abs(a), std::abs(b)));
